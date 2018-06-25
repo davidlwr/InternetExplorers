6a68d2a7d12bc26e9bd5adaf490cd9cf1da85035
bricakeld <aldricbek@gmail.com>
2018-06-25 23:11:52 +0800

    Remove duplicates from sysmon data

--- web/apps/input_sysmon.py
+++ web/apps/input_sysmon.py
@@ -5,6 +5,10 @@ import os
 # specify file parameters
 file_folder = '../stbern-20180302-20180523-csv/'
 
+# testing options
+if __name__ == '__main__':
+    pd.options.mode.chained_assignment = None  # default='warn
+
 # initialize empty df
 sysmon_data = pd.DataFrame()
 
@@ -20,3 +24,20 @@ for filename in os.listdir(file_folder):
 # print(sysmon_data.reading_type.unique())
 
 # look for time periods with disconnected
+# first get only disconnected and uptime rows
+sysmon_relevant_data = sysmon_data.loc[sysmon_data['reading_type'].isin(['disconnected','uptime'])]
+
+# remove duplicates
+sysmon_relevant_data.drop_duplicates(['device_id','device_loc','gw_device','gw_timestamp','key','reading_type'], inplace=True)
+
+# get inactive period within time
+def get_disconnected_periods():
+    '''
+    '''
+    return []
+
+# below for testing
+if __name__ == '__main__':
+    # print(sysmon_relevant_data.head())
+    print(sysmon_relevant_data['reading_type'].value_counts())
+    # print(sysmon_relevant_data[sysmon_relevant_data.duplicated(['device_id','device_loc','gw_device','gw_timestamp','key','reading_type'], keep=False)])
