480d2cd50ff5103d25832f9497fe26d24a8eac74
bricakeld <aldricbek@gmail.com>
2018-07-28 09:51:22 +0800

    Serve css from repo
    Added static folder

--- web/app.py
+++ web/app.py
@@ -16,6 +16,6 @@ app.config.suppress_callback_exceptions = True
 
 # Bootstrap sample template for css
 css_source = ['https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
-              'https://codepen.io/bricakeld/pen/qKZeGq.css']
+              '/static/extra.css']
 for css in css_source:
     app.css.append_css({'external_url': css})
