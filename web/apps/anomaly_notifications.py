c2c8fec0d1bbc9cf0083f4fc768519e739f87958
Aldric <aldricbek@gmail.com>
2018-11-12 22:39:05 +0800

    Complete flask endpoints for anomaly notifications

--- web/apps/anomaly_notifications.py
+++ web/apps/anomaly_notifications.py
@@ -11,10 +11,43 @@ def anomaly_notifications():
     cursor = connection.cursor()
     
     try:
-        query_text = ''
+        query_text = 'SELECT * FROM stbern.anomaly WHERE response = 0'
+        cursor.execute(query_text)
+        result = cursor.fetchall()
+        if result != None:
+            return jsonify(result)
+        else:
+            return ''
     except:
         print("Exceptions occurred in anomaly_notifications")
-        raise
+        # raise
         # return some empty json to fail safely
+        return jsonify()
     finally:
         factory.close_all(cursor=cursor, connection=connection)
+        
+    return jsonify(result)
+
+@server.route('/anomaly_notifications_count', methods=['GET', 'POST'])
+def anomaly_notifications_count():
+    factory = connection_manager()
+    connection = factory.connection
+    cursor = connection.cursor()
+    
+    try:
+        query_text = 'SELECT * FROM stbern.anomaly WHERE response = 0'
+        cursor.execute(query_text)
+        result = cursor.fetchall()
+        if result != None:
+            return str(len(result))
+        else:
+            return ''
+    except:
+        print("Exceptions occurred in anomaly_notifications")
+        # raise
+        # return some number to fail safely
+        return str(-1)
+    finally:
+        factory.close_all(cursor=cursor, connection=connection)
+        
+    return str(len(result))
